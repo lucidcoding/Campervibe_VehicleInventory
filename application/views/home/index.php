139d969610568745f4fe22ed2e0ca2139e6ec85e
Paul Davies <pd_account_2@hotmail.com>
2015-07-18 17:03:16 +0100

    Layout tidying

--- application/views/home/index.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <h2>You are in the View: application/views/home/index.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be the homepage.</p>
-</div>
-<div>
-    <a href="https://login.microsoftonline.com/0647f417-1bf4-49be-a7d6-27537bec904f/oauth2/authorize?client_id=2d708142-face-4379-bc2d-7ff4bcfe9be2&response_mode=form_post&response_type=code+id_token&scope=openid+profile&state=af1Efa&nonce=1">
-        Login
-    </a>
-</div>
